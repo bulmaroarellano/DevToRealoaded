d3671b4f136606ba85e108b78e63ed3b3e51f76d
Bulmaro Arellano <tomaro@outlook.es>
2021-07-15 22:50:25 -0500

    add sdk firebase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 var database = firebase.database();
+var article_id = 1;
+    var article = {
+            'title': 'Conectar Firebase con tu app de JavaScript',
+    }
+
+    firebase.database().ref('articles/' + article_id).set(article);
