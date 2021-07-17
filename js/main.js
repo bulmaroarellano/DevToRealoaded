91a2cd432868ac6e6ee20f185b9de96ba20e8c53
Bulmaro Arellano <tomaro@outlook.es>
2021-07-17 01:08:09 -0500

    Se divide la web prinicipal en archivos por seccion

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ let database = firebase.database();
 
 let devtoRef = database.ref("devTools");
 devtoRef.on("value", (snapshot) => {
-  result = snapshot.val();
-  console.log(result);
+    result = snapshot.val();
+    console.log(result);
+});
+
+
+//Inyectamos el html del header
+$(function() {
+    $('header').load('../partials/header.html');
+    $('#sidebar').load('../partials/sidebar.html');
+    $('#sidebar-left').load('../partials/sidebar-left.html');
+    $('#content').load('../partials/content.html');
+    $('#sidebar-right').load('../partials/sidebar-right.html');
+    $('footer').load('../partials/footer.html');
 });
